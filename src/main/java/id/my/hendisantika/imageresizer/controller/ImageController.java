ac7e2ed0e043119e03057d23f54c136c7374bf3a
hendisantika <hendisantika@yahoo.co.id>
2024-11-08 06:12:55 +0700

    Add requestImage in ImageController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/imageresizer/controller/ImageController.java
+++ src/main/java/id/my/hendisantika/imageresizer/controller/ImageController.java
@@ -1,6 +1,11 @@
 package id.my.hendisantika.imageresizer.controller;
 
+import id.my.hendisantika.imageresizer.model.ImageSettingsBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,4 +24,18 @@ public class ImageController {
 
     private final static String SOURCE_IMG_DIR = "https://s3.eu-west-2.amazonaws.com/bucket-10/resources/sourceImages/";
     private final static String RESIZED_IMG_DIR = "https://s3.eu-west-2.amazonaws.com/bucket-10/resources/resizedImages/";
+
+    /**
+     * @param fileName - the name of the image file
+     * @param typeName - the type of the image file (e.g. thumbnail)
+     * @return ResponseEntity - the resized image
+     */
+    @RequestMapping(value = "/image/show/{typeName}/seo/{fileName}")
+    public ResponseEntity<byte[]> requestImage(@PathVariable String fileName, @PathVariable String typeName) {
+        ImageSettingsBuilder imageSettingsBuilder = new ImageSettingsBuilder();
+        imageSettingsBuilder.buildImageSettings(typeName);
+        byte[] resizedImage = this.getResizedImageFile(fileName, typeName);
+
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(resizedImage);
+    }
 }
