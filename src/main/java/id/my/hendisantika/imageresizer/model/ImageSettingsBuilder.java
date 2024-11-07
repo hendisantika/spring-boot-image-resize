4a022a76942c8039ca5ff49877cd323aa568e0b8
hendisantika <hendisantika@yahoo.co.id>
2024-11-08 06:11:19 +0700

    Add buildImageSettings in ImageSettingsBuilder🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/imageresizer/model/ImageSettingsBuilder.java
+++ src/main/java/id/my/hendisantika/imageresizer/model/ImageSettingsBuilder.java
@@ -1,5 +1,7 @@
 package id.my.hendisantika.imageresizer.model;
 
+import java.security.InvalidParameterException;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-image-resize
@@ -13,4 +15,16 @@ package id.my.hendisantika.imageresizer.model;
 public class ImageSettingsBuilder {
     private final static String IMG_TYPE_THUMBNAIL = "thumbnail";
     private final static String IMG_TYPE_DETAIL = "detail";
+
+    public ImageSettings buildImageSettings(String typeName) {
+        if (typeName.equals(IMG_TYPE_THUMBNAIL)) {
+
+            return new ImageSettings(200, 200, 90);
+        } else if (typeName.equals(IMG_TYPE_DETAIL)) {
+
+            return new ImageSettings(400, 400, 90);
+        } else {
+            throw new InvalidParameterException("The requested image type is not available. Valid types: thumbnail, detail ");
+        }
+    }
 }
