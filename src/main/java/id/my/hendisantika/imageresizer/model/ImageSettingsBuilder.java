6b27826a0175d721b773f904b60be324b9518e43
hendisantika <hendisantika@yahoo.co.id>
2024-11-08 06:10:58 +0700

    Add ImageSettingsBuilder🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/imageresizer/model/ImageSettingsBuilder.java
@@ -0,0 +1,16 @@
+package id.my.hendisantika.imageresizer.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-image-resize
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/11/24
+ * Time: 06.10
+ * To change this template use File | Settings | File Templates.
+ */
+public class ImageSettingsBuilder {
+    private final static String IMG_TYPE_THUMBNAIL = "thumbnail";
+    private final static String IMG_TYPE_DETAIL = "detail";
+}
