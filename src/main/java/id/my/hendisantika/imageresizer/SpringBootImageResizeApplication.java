195a2955a7745cfa811911e22ffe386551dc82ad
hendisantika <hendisantika@yahoo.co.id>
2024-11-08 06:05:03 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/imageresizer/SpringBootImageResizeApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.imageresizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootImageResizeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootImageResizeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/imageresizer/SpringBootImageResizeApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.imageresizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootImageResizeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
