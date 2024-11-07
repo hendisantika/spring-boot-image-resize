20483acc387d98b0181bb13c00b70dd7a2de4118
hendisantika <hendisantika@yahoo.co.id>
2024-11-08 06:14:01 +0700

    Add getResizedImageFile in ImageController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/imageresizer/controller/ImageController.java
+++ src/main/java/id/my/hendisantika/imageresizer/controller/ImageController.java
@@ -1,12 +1,19 @@
 package id.my.hendisantika.imageresizer.controller;
 
 import id.my.hendisantika.imageresizer.model.ImageSettingsBuilder;
+import id.my.hendisantika.imageresizer.service.ImageResizer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * Created by IntelliJ IDEA.
@@ -37,5 +44,43 @@ public class ImageController {
         byte[] resizedImage = this.getResizedImageFile(fileName, typeName);
 
         return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(resizedImage);
+    }
+
+    /**
+     * Check if resized file already exists, otherwise resize it
+     *
+     * @param fileName - the name of the image file
+     * @param typeName - the type of the image file (e.g. thumbnail)
+     * @return image - the resized image
+     */
+    private byte[] getResizedImageFile(String fileName, String typeName) {
+        try {
+            URL url = new URL(RESIZED_IMG_DIR + typeName + "/" + typeName + "-" + fileName);
+            BufferedImage resizedBufferedImage = ImageIO.read(url);
+
+            if (resizedBufferedImage != null) {
+                log.info("Optimized image exists");
+                ByteArrayOutputStream os = new ByteArrayOutputStream();
+                ImageIO.write(resizedBufferedImage, "jpg", os);
+
+                return os.toByteArray();
+            }
+        } catch (IOException e) {
+            log.error("The URL is not valid: {}", String.valueOf(e));
+        }
+
+        try {
+            URL sourceImageUrl = new URL(SOURCE_IMG_DIR + fileName);
+            BufferedImage sourceImage = ImageIO.read(sourceImageUrl);
+            log.info("Optimized image does not exists");
+            ImageResizer imageResizer = new ImageResizer();
+
+            return imageResizer.resizeImage(sourceImage, typeName);
+
+        } catch (IOException e) {
+            log.error("The original image does not exist: {}", String.valueOf(e));
+        }
+
+        return null;
     }
 }
